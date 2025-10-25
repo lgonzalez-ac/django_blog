214924f6815533c9ac8257cdfffc854464ede3db
lgonzalez-ac <gonzalez.lenin.ac@gmail.com>
2025-10-25 11:11:05 -0400

    model/blog added

---
+++ blogs/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at", "updated_at")
+    search_fields = ("title", "content", "author__username")
+    list_filter = ("created_at", "updated_at", "author")
+
+
+admin.site.register(Post, PostAdmin)
